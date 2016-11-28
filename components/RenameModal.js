1487aacd40d0cb2cf47bc266745a31a47dfc0517
Vasilis Kortsimelidis <vaskort@gmail.com>
2016-11-28 22:26:29 +0000

    just added the modal component

---
+++ components/RenameModal.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { View, Text, Modal} from 'react-native';
+
+class RenameModal extends React.Component {
+
+  state = {
+    modalVisible: false,
+  }
+
+  setModalVisible(visible) {
+    this.setState({modalVisible: visible});
+  }
+
+  render() {
+    return (
+      <View style={{marginTop: 22}}>
+        <Modal
+          animationType={"slide"}
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {alert("Modal has been closed.")}}
+          >
+         <View style={{marginTop: 22}}>
+          <View>
+            <Text>Hello World!</Text>
+
+            <TouchableHighlight onPress={() => {
+              this.setModalVisible(!this.state.modalVisible)
+            }}>
+              <Text>Hide Modal</Text>
+            </TouchableHighlight>
+
+          </View>
+         </View>
+        </Modal>
+
+        <TouchableHighlight onPress={() => {
+          this.setModalVisible(true)
+        }}>
+          <Text>Show Modal</Text>
+        </TouchableHighlight>
+
+      </View>
+    );
+  }
+
+}
+
+module.exports = RenameModal;
