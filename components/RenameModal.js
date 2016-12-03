c46926dbb473293ab70e2a4eeeea46bf908dedee
Vasilis Kortsimelidis <vaskort@gmail.com>
2016-12-03 18:26:59 +0000

    added save cancel button with styling but not fully functional yet

--- components/RenameModal.js
+++ components/RenameModal.js
@@ -1,42 +1,74 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableHighlight, StyleSheet} from 'react-native';
-import Modal from 'react-native-modalbox';
+import { View, Text, Modal, TouchableHighlight, StyleSheet} from 'react-native';
+import { Button } from 'react-native-elements';
 
 class RenameModal extends React.Component {
 
   render() {
     return (
-      <View style={{marginTop: 22}}>
         <Modal
           animationType={"fade"}
           transparent={true}
-          isOpen={this.props.isVisible}
+          visible={this.props.isVisible}
           onRequestClose={() => {alert("Modal has been closed.")}}
           >
-         <View style={{marginTop: 22}}>
+         <View style={styles.modalWrapper}>
           <View style={styles.modal}>
-            <Text>{this.props.locationName}</Text>
-
-            <TouchableHighlight onPress={() => {
-              this.props.setModalVisible(false)
-            }}>
-              <Text>Hide Modal</Text>
-            </TouchableHighlight>
+            <Text style={styles.locationName}>{this.props.locationName}</Text>
+            <View style={styles.buttonWrapper}>
+              <Button
+                raised
+                title="Save"
+                onPress={() => {
+                  this.props.setModalVisible(false)
+                }}
+                buttonStyle={styles.buttons}
+              />
+              <Button
+                raised
+                title="Cancel"
+                onPress={() => {
+                  this.props.setModalVisible(false)
+                }}
+                buttonStyle={styles.buttons}
+              />
+            </View>
           </View>
          </View>
         </Modal>
-      </View>
     );
   }
 
 }
 
 const styles = StyleSheet.create({
+  modalWrapper: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.5)'
+  },
   modal: {
-    backgroundColor: 'rgba(0,0,0,0.5)',
-    // flex: 1,
-    // flexDirection: 'row',
-    alignSelf: 'stretch'
+    backgroundColor: 'white',
+    alignSelf: 'stretch',
+    height: 100,
+    padding: 5
+  },
+  buttons: {
+    borderRadius: 5,
+    paddingLeft: 20,
+    paddingRight: 20
+  },
+  locationName: {
+    flex:1,
+    justifyContent: 'center',
+  },
+  buttonWrapper: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
   }
 });
 
